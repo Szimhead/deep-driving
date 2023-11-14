b5191bc8f71b3b11c0d8f7d048fc5014bfc160e4
Szymon Zmijewski <sz.zmijewski24@gmail.com>
2023-11-14 12:04:14 +0100

    unet implementation

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,6 +1,12 @@
 import os
 import numpy as np
 from PIL import Image
+from jproperties import Properties
+import matplotlib.pyplot as plt
+import random
+
+SEED = 42
+
 
 def load_numpy_arrays(folder_path):
     black_cars = []
@@ -17,11 +23,11 @@ def load_numpy_arrays(folder_path):
             else:
                 test.append(array)
 
-            
     black_cars = np.array(black_cars)
     orange_cars = np.array(orange_cars)
     test = np.array(test)
-    return black_cars,orange_cars,test
+    return black_cars, orange_cars, test
+
 
 def process_image(image_path):
     img = Image.open(image_path)
@@ -31,58 +37,127 @@ def process_image(image_path):
     landscape_array = np.array(img)
     return landscape_array
 
-# Specify the path to your folder containing NumPy arrays
-folder_path = 'C:\\Users\\aless\\OneDrive - Danmarks Tekniske Universitet\\Period_13_1\\DL\\project\\carseg_data\\carseg_data\\arrays'
-landscapes_folder = 'C:\\Users\\aless\\OneDrive - Danmarks Tekniske Universitet\\Period_13_1\\DL\\project\\carseg_data\\carseg_data\\images\\landscapes'
 
-# Sample mapping dictionary
-class_mapping = {
-    10: ("orange", "hood"),
-    20: ("dark green", "front door"),
-    30: ("yellow", "rear door"),
-    40: ("cyan", "frame"),
-    50: ("purple", "rear quater panel"),
-    60: ("light green", "trunk lid"),
-    70: ("blue", "fender"),
-    80: ("pink", "bumper"),
-    90: ("no color", "rest of car")
-}
-
-
-b_cars,o_cars, test = load_numpy_arrays(folder_path)
-info = []
-for channels in b_cars:
-    target_channel = channels[:,:,3]
-    target_info = np.vectorize(class_mapping.get)(target_channel)
-    info.append(target_channel)
-
-all_landscape_arrays = []
-# Iterate over all files in the "landscapes" folder
-for file_name in os.listdir(landscapes_folder):
-    if file_name.endswith('.jpg'):
-        # Construct the full path to the image
-        image_path = os.path.join(landscapes_folder, file_name)
-        # Process the image and append the resulting array to the list
-        landscape_ar = process_image(image_path)
-        all_landscape_arrays.append(landscape_ar)
+# all_landscape_arrays = []
+# # Iterate over all files in the "landscapes" folder
+# for file_name in os.listdir(landscapes_folder):
+#     if file_name.endswith('.jpg'):
+#         # Construct the full path to the image
+#         image_path = os.path.join(landscapes_folder, file_name)
+#         # Process the image and append the resulting array to the list
+#         landscape_ar = process_image(image_path)
+#         all_landscape_arrays.append(landscape_ar)
 
 # Convert the list to a NumPy array if needed
-#all_landscape_arrays = np.array(all_landscape_arrays)
-j=0
+# all_landscape_arrays = np.array(all_landscape_arrays)
+# j = 0
 
-for landscape_array in all_landscape_arrays:
-    j+=1
-    for t in range(len(b_cars)):
-        new_bkg = np.zeros_like(landscape_array)
-        target_channel_array = info[t]
-        target_zero_mask = target_channel_array == 0
-        for k in range(2):
-            new_bkg[:, :, k] = np.where(target_zero_mask, landscape_array[:, :, k], b_cars[t,:, :, k])
+# for landscape_array in all_landscape_arrays:
+#     j += 1
+#     for t in range(len(b_cars)):
+#         new_bkg = np.zeros_like(landscape_array)
+#         target_channel_array = info[t]
+#         target_zero_mask = target_channel_array == 0
+#         for k in range(2):
+#             new_bkg[:, :, k] = np.where(target_zero_mask, landscape_array[:, :, k], b_cars[t, :, :, k])
+#
+#         new_bkg_uint8 = np.uint8(new_bkg)
+#         new_image = Image.fromarray(new_bkg_uint8)
+#         image_path = 'C:\\Users\\aless\\OneDrive - Danmarks Tekniske Universitet\\Period_13_1\\DL\\project\\carseg_data\\carseg_data\\images\\black_5_doors_landscape'
+#         image_name = str(t + 1) + '_' + str(j) + '.jpg'
+#         file_path = os.path.join(image_path, image_name)
+#         # Save the image to a file
+#         new_image.save(file_path)
 
-        new_bkg_uint8 = np.uint8(new_bkg)
-        new_image = Image.fromarray(new_bkg_uint8)
-        image_path = 'C:\\Users\\aless\\OneDrive - Danmarks Tekniske Universitet\\Period_13_1\\DL\\project\\carseg_data\\carseg_data\\images\\black_5_doors_landscape'
-        image_name = str(t+1) + '_'+str(j)+'.jpg'
-        file_path = os.path.join(image_path, image_name)
-        # Save the image to a file
-        new_image.save(file_path)
+def get_data():
+    configs = Properties()
+    with open('application.properties', 'rb') as read_prop:
+        configs.load(read_prop)
+    b_cars, o_cars, test = load_numpy_arrays(configs.get("FOLDER_PATH").data)
+    masks = []
+    images = []
+    for channels in b_cars:
+        target_channel = channels[:, :, 3]
+        image = channels[:, :, :3]
+        masks.append(target_channel)
+        images.append(image)
+    random.Random(SEED).shuffle(images)
+    random.Random(SEED).shuffle(masks)
+    return images, masks
+
+
+if __name__ == '__main__':
+    configs = Properties()
+    with open('application.properties', 'rb') as read_prop:
+        configs.load(read_prop)
+
+
+    # Specify the path to your folder containing NumPy arrays
+    # folder_path = 'C:\\Users\\aless\\OneDrive - Danmarks Tekniske Universitet\\Period_13_1\\DL\\project\\carseg_data\\carseg_data\\arrays'
+    # landscapes_folder = 'C:\\Users\\aless\\OneDrive - Danmarks Tekniske Universitet\\Period_13_1\\DL\\project\\carseg_data\\carseg_data\\images\\landscapes'
+
+    # Sample mapping dictionary
+    class_mapping = {
+        10: ("orange", "hood"),
+        20: ("dark green", "front door"),
+        30: ("yellow", "rear door"),
+        40: ("cyan", "frame"),
+        50: ("purple", "rear quater panel"),
+        60: ("light green", "trunk lid"),
+        70: ("blue", "fender"),
+        80: ("pink", "bumper"),
+        90: ("no color", "rest of car")
+    }
+
+    b_cars, o_cars, test = load_numpy_arrays(configs.get("FOLDER_PATH").data)
+    masks = []
+    images = []
+    for channels in b_cars:
+        target_channel = channels[:, :, 3]
+        image = channels[:, :, :3]
+        target_info = np.vectorize(class_mapping.get)(target_channel)
+        masks.append(target_channel)
+        images.append(image)
+
+    plt.figure(figsize=(18, 6))
+    plt.imshow(target_channel)
+
+    print(target_channel)
+
+    im = Image.open(configs.get("BLACK_MASK").data+"\\0001.png")
+    from collections import defaultdict
+    by_color = defaultdict(int)
+    for pixel in im.getdata():
+        by_color[pixel] += 1
+
+    print(list(filter(lambda x: by_color[x] > 200, by_color)))
+
+    amount = 5
+    image_sample = random.choices(b_cars, k=amount)
+    resized_images = []
+    raw_images = []
+
+    # Define figure size
+    fig = plt.figure(figsize=(18, 6))
+
+    # Save original images in the figure
+    # ax = plt.subplot(2, amount + 1, 1)
+    # txt = ax.text(0.4, 0.5, 'Original', fontsize=20)
+    # txt.set_clip_on(False)
+
+    plt.axis('off')
+    for i, image in enumerate(image_sample):
+        i += len(image_sample) + 3
+        plt.subplot(2, amount + 1, i)
+        # image = imread(path, as_gray=True)
+        # ret, thresh = cv.threshold(image, 0.95, 1, cv.THRESH_BINARY)
+        #
+        # cropped = pad2square(image, thresh)  # Make the image square
+        # image = resize(image, output_shape=image_size, mode='reflect', anti_aliasing=True)  # resizes the image
+        # resized_images.append(cropped)
+        # raw_images.append(image)
+
+        plt.imshow(image)
+
+    # Show plot
+    plt.show()
---
+++ train.py
@@ -0,0 +1,175 @@
+import os
+
+from jproperties import Properties
+
+from data_augmentation import get_data
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+import numpy as np
+import pandas as pd
+import cv2
+from glob import glob
+import scipy.io
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, CSVLogger
+from unet import build_unet
+
+""" Global parameters """
+global IMG_H
+global IMG_W
+global NUM_CLASSES
+global CLASSES
+global COLORMAP
+
+""" Creating a directory """
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+""" Load and split the dataset """
+
+
+def load_dataset(path, split=0.2):
+    train_x, train_y = get_data()
+
+    split_size = int(split * len(train_x))
+
+    train_x, valid_x = train_test_split(train_x, test_size=split_size, random_state=42)
+    train_y, valid_y = train_test_split(train_y, test_size=split_size, random_state=42)
+
+    train_x, test_x = train_test_split(train_x, test_size=split_size, random_state=42)
+    train_y, test_y = train_test_split(train_y, test_size=split_size, random_state=42)
+
+    return (train_x, train_y), (valid_x, valid_y), (test_x, test_y)
+
+
+def get_colormap():
+    colormap = np.array(
+        [
+            10,
+            20,
+            30,
+            40,
+            50,
+            60,
+            70,
+            80,
+            90
+         ])
+
+    classes = [
+        "hood",
+        "front door",
+        "rear door",
+        "frame",
+        "rear quater panel",
+        "trunk lid",
+        "fender",
+        "bumper",
+        "rest of car"
+    ]
+
+    return classes, colormap
+
+
+def read_image_mask(x, y):
+    """ Image processing """
+    x = x / 255.0
+    x = x.astype(np.float32)
+
+    """ Mask processing """
+    output = []
+    for color in COLORMAP:
+        cmap = np.equal(y, color)
+        output.append(cmap)
+    output = np.stack(output, axis=-1)
+    output = output.astype(np.uint8)
+
+    return x, output
+
+
+def preprocess(x, y):
+    def f(x, y):
+        image, mask = read_image_mask(x, y)
+        return image, mask
+
+    image, mask = tf.numpy_function(f, [x, y], [tf.float32, tf.uint8])
+    image.set_shape([IMG_H, IMG_W, 3])
+    mask.set_shape([IMG_H, IMG_W, NUM_CLASSES])
+
+    return image, mask
+
+
+def tf_dataset(x, y, batch=8):
+    dataset = tf.data.Dataset.from_tensor_slices((x, y))
+    dataset = dataset.shuffle(buffer_size=5000)
+    dataset = dataset.map(preprocess)
+    dataset = dataset.batch(batch)
+    dataset = dataset.prefetch(2)
+    return dataset
+
+
+if __name__ == "__main__":
+    """ Seeding """
+    np.random.seed(42)
+    tf.random.set_seed(42)
+
+    """ Directory for storing files """
+    create_dir("files")
+
+    """ Hyperparameters """
+    IMG_H = 256
+    IMG_W = 256
+    NUM_CLASSES = 9
+    input_shape = (IMG_H, IMG_W, 3)
+
+    batch_size = 16
+    lr = 1e-8
+    num_epochs = 20
+    configs = Properties()
+    with open('application.properties', 'rb') as read_prop:
+        configs.load(read_prop)
+    dataset_path = configs.get("BLACK").data
+
+    model_path = os.path.join("files", "model.h5")
+    csv_path = os.path.join("files", "data.csv")
+
+    """ Loading the dataset """
+    (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_dataset(dataset_path)
+    print(
+        f"Train: {len(train_x)}/{len(train_y)} - Valid: {len(valid_x)}/{len(valid_y)} - Test: {len(test_x)}/{len(test_x)}")
+    print("")
+
+    """ Process the colormap """
+    CLASSES, COLORMAP = get_colormap()
+
+    """ Dataset Pipeline """
+    train_dataset = tf_dataset(train_x, train_y, batch=batch_size)
+    valid_dataset = tf_dataset(valid_x, valid_y, batch=batch_size)
+
+    """ Model """
+    model = build_unet(input_shape, NUM_CLASSES)
+    model.load_weights(model_path)
+    model.compile(
+        loss="categorical_crossentropy",
+        optimizer=tf.keras.optimizers.Adam(lr)
+    )
+    # model.summary()
+
+    """ Training """
+    callbacks = [
+        ModelCheckpoint(model_path, verbose=1, save_best_only=True),
+        ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=5, min_lr=1e-7, verbose=1),
+        CSVLogger(csv_path, append=True),
+        EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=False)
+    ]
+
+    model.fit(train_dataset,
+              validation_data=valid_dataset,
+              epochs=num_epochs,
+              callbacks=callbacks
+              )
